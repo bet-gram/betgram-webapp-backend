c34feb8b91b882cddf5707288cda7d41c673a1f6
scvalencia <sc.valencia606@uniandes.edu.co>
2015-11-09 08:58:41 -0500

    New db schema and so

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,206 +3,91 @@
 
 # --- !Ups
 
-create table betting_odd (
-  id                        bigserial not null,
-  constraint pk_betting_odd primary key (id))
-;
-
-create table blog (
-  id                        bigserial not null,
-  constraint pk_blog primary key (id))
-;
-
-create table city (
-  id                        bigserial not null,
-  name                      varchar(255),
-  website                   varchar(255),
-  constraint pk_city primary key (id))
-;
-
-create table club (
-  id                        bigserial not null,
-  full_name                 varchar(255),
-  founded                   timestamp,
-  owner                     varchar(255),
-  website                   varchar(255),
-  betgram_code              varchar(255),
-  constraint pk_club primary key (id))
-;
-
-create table confederation (
-  id                        bigserial not null,
-  name                      varchar(255),
-  founded                   timestamp,
-  website                   varchar(255),
-  constraint pk_confederation primary key (id))
-;
-
-create table country (
-  id                        bigserial not null,
-  name                      varchar(255),
-  national_language         varchar(255),
-  currency                  varchar(255),
-  constraint pk_country primary key (id))
+create table climate (
+  city                      varchar(255) not null,
+  weather                   varchar(255),
+  temp                      varchar(255),
+  feelslike                 varchar(255),
+  wind                      varchar(255),
+  constraint pk_climate primary key (city))
 ;
 
 create table ground (
   id                        bigserial not null,
   name                      varchar(255),
+  city                      varchar(255),
+  latitude                  REAL,
+  longitude                 REAL,
   capacity                  integer,
-  record_attendance         integer,
-  opened                    timestamp,
   constraint pk_ground primary key (id))
 ;
 
-create table human (
+create table historic_data (
   id                        bigserial not null,
-  name                      varchar(255),
-  date_of_birth             timestamp,
-  constraint pk_human primary key (id))
-;
-
-create table league (
-  id                        bigserial not null,
-  founded                   timestamp,
-  name                      varchar(255),
-  website                   varchar(255),
-  constraint pk_league primary key (id))
-;
-
-create table payment (
-  id                        bigserial not null,
-  amount                    float,
-  constraint pk_payment primary key (id))
-;
-
-create table position (
-  id                        bigserial not null,
-  name                      varchar(255),
-  constraint pk_position primary key (id))
-;
-
-create table statistic (
-  id                        bigserial not null,
-  home                      varchar(255),
-  away                      varchar(255),
-  league                    varchar(255),
+  date_value                integer,
+  hometeam                  varchar(255),
+  awayteam                  varchar(255),
   fthg                      integer,
   ftag                      integer,
-  ftr                       varchar(255),
-  hthg                      integer,
-  htag                      integer,
-  htr                       varchar(255),
-  attendance                integer,
-  referee                   varchar(255),
-  hs                        integer,
-  ashots                    integer,
-  hst                       integer,
-  ast                       integer,
-  hhw                       integer,
-  ahw                       integer,
-  hc                        integer,
-  ac                        integer,
-  hf                        integer,
-  aw                        integer,
-  ho                        integer,
-  ao                        integer,
-  hy                        integer,
-  ay                        integer,
-  hr                        integer,
-  ar                        integer,
-  hbp                       integer,
-  abp                       integer,
-  b365h                     REAL,
-  b365d                     REAL,
-  b365a                     REAL,
-  bsh                       REAL,
-  bsd                       REAL,
-  bsa                       REAL,
-  bwh                       REAL,
-  bwd                       REAL,
-  bwa                       REAL,
-  gbh                       REAL,
-  gbd                       REAL,
-  gba                       REAL,
-  iwh                       REAL,
-  iwd                       REAL,
-  iwa                       REAL,
-  lbh                       REAL,
-  lbd                       REAL,
-  lba                       REAL,
-  psh                       REAL,
-  psd                       REAL,
-  psa                       REAL,
-  soh                       REAL,
-  sod                       REAL,
-  soa                       REAL,
-  sbh                       REAL,
-  sbd                       REAL,
-  sba                       REAL,
-  sjh                       REAL,
-  sjd                       REAL,
-  sja                       REAL,
-  syh                       REAL,
-  syd                       REAL,
-  sya                       REAL,
-  vch                       REAL,
-  vcd                       REAL,
-  vca                       REAL,
-  whh                       REAL,
-  whd                       REAL,
-  wha                       REAL,
-  bb1x2                     integer,
-  bb_mx_h                   REAL,
-  bb_av_h                   REAL,
-  bb_mx_d                   REAL,
-  bb_av_d                   REAL,
-  bb_mx_a                   REAL,
-  bb_av_a                   REAL,
-  bb_ou                     integer,
-  bb_mx_more                REAL,
-  bb_av_more                REAL,
-  bb_mx_less                REAL,
-  bb_av_less                REAL,
-  gbmore                    REAL,
-  gbless                    REAL,
-  b365more                  float,
-  b365less                  REAL,
-  bb_ah                     REAL,
-  bb_ahh                    REAL,
-  bb_mx_ahh                 REAL,
-  bb_av_ahh                 REAL,
-  bb_mx_aha                 REAL,
-  bb_av_aha                 REAL,
-  constraint pk_statistic primary key (id))
+  fthc                      integer,
+  ftac                      integer,
+  amph                      REAL,
+  ampa                      REAL,
+  constraint pk_historic_data primary key (id))
 ;
 
+create table match (
+  id                        bigserial not null,
+  date                      timestamp,
+  home_betgram_code         varchar(255),
+  away_betgram_code         varchar(255),
+  home_goals                integer,
+  away_goals                integer,
+  winner                    varchar(255),
+  location                  varchar(255),
+  image                     varchar(255),
+  past                      boolean,
+  constraint pk_match primary key (id))
+;
+
+create table metrics (
+  team                      varchar(255) not null,
+  follower_count            integer,
+  retweet_count             integer,
+  weighted_retweet_index    REAL,
+  constraint pk_metrics primary key (team))
+;
+
+create table team (
+  betgram_code              varchar(255) not null,
+  name                      varchar(255),
+  ground_id                 bigint,
+  website                   varchar(255),
+  logo                      varchar(255),
+  constraint uq_team_ground_id unique (ground_id),
+  constraint pk_team primary key (betgram_code))
+;
+
+alter table match add constraint fk_match_home_1 foreign key (home_betgram_code) references team (betgram_code);
+create index ix_match_home_1 on match (home_betgram_code);
+alter table match add constraint fk_match_away_2 foreign key (away_betgram_code) references team (betgram_code);
+create index ix_match_away_2 on match (away_betgram_code);
+alter table team add constraint fk_team_ground_3 foreign key (ground_id) references ground (id);
+create index ix_team_ground_3 on team (ground_id);
 
 
 
 # --- !Downs
 
-drop table if exists betting_odd cascade;
-
-drop table if exists blog cascade;
-
-drop table if exists city cascade;
-
-drop table if exists club cascade;
-
-drop table if exists confederation cascade;
-
-drop table if exists country cascade;
+drop table if exists climate cascade;
 
 drop table if exists ground cascade;
 
-drop table if exists human cascade;
+drop table if exists historic_data cascade;
 
-drop table if exists league cascade;
+drop table if exists match cascade;
 
-drop table if exists payment cascade;
+drop table if exists metrics cascade;
 
-drop table if exists position cascade;
-
-drop table if exists statistic cascade;
+drop table if exists team cascade;
 
